97e7eff33971056497c225de2c66cfcc7266ba39
daniel cardoso <danielggcardoso@gmail.com>
2026-06-02 09:04:11 -0300

    refactor ex39 function to use async and improve sum calculation

--- exercicioss/exercicio39.js
+++ exercicioss/exercicio39.js
@@ -1,4 +1,4 @@
-export function ex39(rl, exibirMenu) {
+export async function ex39(rl, exibirMenu) {
     console.clear();
     console.log("===============================================================================")
     console.log("-----------EXIBIR A SOMA DOS NÚMEROS INTEIROS POSITIVOS DE 1 A 100-------------")
@@ -7,7 +7,7 @@ export function ex39(rl, exibirMenu) {
     let soma = 0
 
     for (let i = 1; i <= 100; i++) {
-        console.log(soma += i)
+        console.log(soma = soma + i)
     }
 
     rl.question("Pressione ENTER para ver a prôxima tabuada...", () => {
