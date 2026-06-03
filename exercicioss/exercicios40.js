a0a15ec6d76b4eff71778ef613fc135fef4c6021
daniel cardoso <danielggcardoso@gmail.com>
2026-06-03 08:17:15 -0300

    add ex40 function to display the first 30 values of the Fibonacci sequence

---
+++ exercicioss/exercicios40.js
@@ -0,0 +1,18 @@
+export function ex40() {
+    console.clear();
+    console.log("=============================================================================")
+    console.log("-----------EXIBIR OS 30 PRIMEIROS VALORES DA SEQUENCIA FIBONACCI-------------")
+    console.log("=============================================================================")
+
+    let fibonacci2 = Array(30)
+    fibonacci2[0] = 0
+    fibonacci2[1] = 1
+
+    for (let i = 2; i <= 29; i++) {
+        fibonacci2[i] = fibonacci2[i - 1] + fibonacci2[i - 2]
+    }
+
+    for (let valor of fibonacci2) {
+        console.log(valor)
+    }
+}
