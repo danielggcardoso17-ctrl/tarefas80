464de6f926cfc5d6ce30afc5240109008bf32ff6
daniel cardoso <danielggcardoso@gmail.com>
2026-06-11 10:50:52 -0300

    refactor ex56 function to improve user prompts and clarify output messages

--- exercicioss/exercicio56.js
+++ exercicioss/exercicio56.js
@@ -2,15 +2,15 @@ const pergunta = (rl, texto) => new Promise((resolve) => rl.question(texto, reso
 
 export async function ex56(rl, exibirMenu) {
     console.clear()
-    console.log("==============================================================")
-    console.log("-----------EXIBIR UM ARRAY APÓS UMA MULTIPLICAÇÃO-------------")
-    console.log("==============================================================")
+    console.log("======================================================================")
+    console.log("-----------EXIBIR UM NÚMERO DE UM ARRAY ATRAVÉS DO ÍNDICE-------------")
+    console.log("======================================================================")
 
     let x = parseFloat(await pergunta(rl, "Digite a quantidade de valores que voce quer digitar: "))
     let array = []
 
     if (isNaN(x) || x > 20 || x <= 0) {
-        console.log("A quantidade deve ser menor ou igual a 20")
+        console.log("A quantidade deve ser menor ou igual a 20 e não negativa")
         x = await pergunta(rl, "Digite uma quantidade válida: ")
     } else {
         for (let i = 0; i < x; i++) {
@@ -25,15 +25,18 @@ export async function ex56(rl, exibirMenu) {
     }
 
     let indice = parseFloat(await pergunta(rl, "consulte um valor desse conjunto de números digitando seu indice: "))
-
-    if (isNaN(indice) || indice < 0 || indice > x) {
+    if (isNaN(indice) || indice < 0 || indice >= x) {
         console.log("valor nao encontrado")
         await pergunta(rl, `Pressione ENTER para tentar novamente...`)
         ex56(rl, exibirMenu)
     } else {
-        console.log(`O elemento é: ${array[indice]}`)
+        console.log(`O elemento é o número: ${array[indice]}`)
     }
 
-    await pergunta(rl, "Pressione ENTER para voltar ao menu...")
-    exibirMenu()
+    let p = await pergunta(rl, "Você deseja fazer uma nova consulta? (S ou N): ")
+
+    while (p === "S" || p === "s" || p === "sim") {
+        ex56(rl, exibirMenu)
+        break;
+    }
 }
